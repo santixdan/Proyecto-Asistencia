30c9b09aaef3f7d3636bae67165f942e2de4dfc2
santixdan <danysilva.2202@gmail.com>
2024-09-02 19:56:00 -0500

    updated

--- asistencia BackEnd/src/models/server.js
+++ asistencia BackEnd/src/models/server.js
@@ -19,7 +19,7 @@ class Server {
     middlewares() {
         this.app.use(express.json());
         this.app.use(cors())
-        // this.app.use(express.static('public'));
+        this.app.use(express.static('public'));
     }
     routes() {
         this.app.use(this.paths.aprendices, require('./../routers/aprendices.js'));
