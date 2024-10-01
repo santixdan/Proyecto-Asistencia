86015fcb589056fda38a4c0d08e605eafdf5d192
Danielito <152442183+santixdan@users.noreply.github.com>
2024-10-01 09:32:00 -0500

    Update aprendices.js

--- asistencia BackEnd/src/controllers/aprendices.js
+++ asistencia BackEnd/src/controllers/aprendices.js
@@ -66,7 +66,7 @@ const httpAprendices = {
             const nombre = req.body.nombre?.trim();
             const telefono = req.body.telefono?.trim();
             const email = req.body.email?.trim();
-            const firma = req.body.firma?.trim();
+            const firma = req.file ? req.file.path : null;
 
             const aprendiz = await Aprendiz.findById(id);
 
